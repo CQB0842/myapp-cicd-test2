e1c8d70a69d3e67264347552574cdf8d8db38e3a
pc-cui <c794218113@gmail.com>
2025-05-01 22:10:51 +0900

    app.py CuiQibin

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello from CI/CD pipeline!'
+    return 'Hello CuiQiBin from CI/CD pipeline!'
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 80))
